f0192baa146ad53313e52420b9be75979a7c6486
CyradisNyx <sarahbeavan@gmail.com>
2017-04-17 13:08:59 +1200

    Docstring Heaven
    Added more detailed docstrings, new requirements.txt, better template
    for movies

--- homeserver/models.py
+++ homeserver/models.py
@@ -16,47 +16,88 @@ class videofile(db.Model):
     video_codec = db.Column(db.String(10))
 
     def __init__(self, file_path, imdb_key=None):
-        """Constructor Method."""
+        """
+        Constructor Method, assign instance variables.
+
+        Set file_path based on input, parse file name to find specific metadata
+        and assign to variables. Assign imdb_key to instance variable, or find
+        if None. Check if videofile is a movie or an episode, then if moviedata
+        or seriesdata for videofile already exists, else build.
+
+        @str file_path: Full path to specific videofile
+        @str imdb_key: IMDB Key corresponding to movie in videofile
+
+        @return new row in db
+        """
         self.file_path = file_path
+        """@str Absolute Path to File."""
+
         data = PTN.parse(self.file_path.rsplit("/", 1)[-1])
+        """@dict Parsing Result from File Path."""
 
         if 'container' in data:
             self.file_type = data['container']
+            """@str File extension, ie ".mkv" or ".avi"."""
         if 'resolution' in data:
             self.file_resolution = data['resolution']
+            """@str Video Resolution, ie "1080p" or "720p"."""
         if 'audio' in data:
             self.audio_codec = data['audio']
+            """@str Audio Codec, ie "DTS" or "FLAC"."""
         if 'codec' in data:
             self.video_codec = data['codec']
+            """@str Video Codec, ie "x264" or "HEVC"."""
 
         if imdb_key is None:
-            imdb_key = self.FindID(file_path, data)
+            imdb_key = self.FindID(data)
 
-        if moviedata.query.get(imdb_key) is None:
+        if moviedata.query.get(imdb_key) is None and self.CheckType(imdb_key) == 'movie':
             db.session.add(moviedata(imdb_key=imdb_key))
             db.session.commit()
 
+        elif seriesdata.query.get(imdb_key) is None and self.CheckType(imdb_key) == 'episode':
+            db.session.add(seriesdata(imdb_key=imdb_key))
+            db.session.commit()
+
         self.imdb_key = imdb_key
+        """@str Key to corresponding IMDB Entry."""
 
     def __repr__(self):
         """Return Pretty Formatted Summary of Model."""
         return '<videofile:{}, imdb_key:{}>'.format(self.file_path, self.imdb_key)
 
-    def FindID(self, Path, data):
-        """Parse File Name and Output imdb_key."""
+    def FindID(self, data):
+        """
+        Parse File Name and Output imdb_key.
+
+        Check if videofile is a movie, or an episode in a larger series. Use
+        data from parsed filename to search IMDB database and find ID.
+
+        @dict data: Data from parsing filename
+
+        @return str valid corresponding imdb_key
+        """
         if 'season' in data:
             episode = omdb.get(episode=data['episode'], season=data['season'], title=data['title'])
             return episode.imdb_id
         else:
             if 'year' in data:
-                moardata = omdb.get(title=data['title'], year=data['year'])
+                movie = omdb.get(title=data['title'], year=data['year'])
             else:
-                moardata = omdb.get(title=data['title'])
+                movie = omdb.get(title=data['title'])
 
-            return moardata.imdb_id
+            return movie.imdb_id
 
     def CheckType(self, imdb_key):
-        """Check if Movie or Episode."""
+        """
+        Check if Movie or Episode.
+
+        Uses IMDB Api Search by imdb_key to determine video type.
+
+        @str imdb_key: key to look up
+
+        @return 'movie' or 'episode'
+        """
         title = omdb.imdbid(imdb_key)
         return title.type
 
@@ -70,31 +111,48 @@ class moviedata(db.Model):
     cover_pic = db.Column(db.String(255))
     release_date = db.Column(db.Integer)
     film_description = db.Column(db.String(255))
-    star_rating = db.Column(db.Float)    # Out of 10
-    runtime = db.Column(db.Integer)  # In Minutes
-    watched = db.Column(db.Integer)  # 0=unwatched, 1=inprogress, 2=watched
+    star_rating = db.Column(db.Float)
+    runtime = db.Column(db.Integer)
+    watched = db.Column(db.Integer)
 
     def __init__(self, imdb_key):
-        """Constructor Method and Populate Fields."""
+        """
+        Constructor Method and assign instance variables.
+
+        Set imdb_key based on declaration input, run an imdb search for the
+        key and then parse information into specific fields.
+
+        @str imdb_key: IMDB Key Corresponding to model
+
+        @return new row in db
+        """
         self.imdb_key = imdb_key
+        """@str Key to Corresponding IMDB Entry."""
         self.watched = 0
+        """@int Indicator of whether or not video has been watched
+        0 = unwatched, 1= in progress, 2 = watched."""
 
         title = omdb.imdbid(imdb_key)
 
         self.movie_title = title.title
+        """@str Title of Movie."""
         self.cover_pic = title.poster
+        """@str URL for Cover Image."""
         self.release_date = int(title.year)
+        """@int Year of release."""
         self.film_description = title.plot
+        """@str Short Summary Description."""
         self.star_rating = float(title.imdb_rating)
+        """@float Aggregated IMDB Rating out of 10."""
         self.runtime = int(title.runtime.split(" ")[0])
+        """@int Runtime in minutes."""
 
     def __repr__(self):
         """Return Pretty Formatted Summary of Model."""
         return '<moviedata:{}>'.format(self.movie_title)
 
 
-'''
-class IMDBSeriesData(db.Model):
+class seriesdata(db.Model):
     """Store IMDB Data for TV Shows."""
 
     imdb_key = db.Column(db.String(30), primary_key=True)
@@ -103,8 +161,6 @@ class IMDBSeriesData(db.Model):
     SeriesDescription = db.Column(db.Text)
     star_rating = db.Column(db.Float)
     NumberOfSeasons = db.Column(db.Integer)
-    Seasons = db.relationship('IMDBSeasonData',
-                              backref='Series',)
+    # Seasons = db.Column()
     NumberOfEpisodes = db.Column(db.Integer)
     watched = db.Column(db.Integer)
-'''
--- homeserver/streaming.py
+++ homeserver/streaming.py
@@ -7,7 +7,19 @@ import subprocess
 
 
 def transcode(path):
-    """Transmux videos into mp4."""
+    """
+    Transmux videos into mp4.
+
+    Check if temp converted file already exists (using temp to seed back),
+    and delete. Call shell subprocess, using ffmpeg to remux into mp4 container
+    At the moment, it's not using a temp directory, to save space on my
+    harddrive, however, it's fairly easy to re-implement. Change the subprocess
+    call to save to app.config['TEMP_DIR'] + converted.mp4, like in the check.
+
+    @str path: Path to Video File (abspath? file name?)
+
+    @return none
+    """
     if os.path.isfile(app.config['TEMP_DIR'] + "converted.mp4"):
         os.remove(app.config['TEMP_DIR'] + "converted.mp4")
     subprocess.call(("ffmpeg -i " +
@@ -17,7 +29,19 @@ def transcode(path):
 
 
 def airplay_background(video):
-    """Stream videos to Airplay device."""
+    """
+    Stream videos to Airplay device. MAYBE DOESNT WORK ANYMORE BECAUSE DB!!!!!.
+
+    Currently hardcoding the AirPlay IP, rather than finding it dynamically.
+    Check if transmuxing is required, if so, call transcode(). If using
+    TEMP_DIR in transmuxing, make sure video is changed to that path. Play
+    using MEDIA_URL. While loop to check for any device interferences, since
+    it's running in a background thread.
+
+    @str video: Path to Video File (abspath? file name?)
+
+    @return none
+    """
     ap = AirPlay('10.0.0.22')
     if video[-4:] != ".mp4":
         transcode(video)
@@ -32,6 +56,14 @@ def airplay_background(video):
 
 
 def localplay(video):
-    """Stream videos to client device."""
+    """
+    Get Video URL for Localplay.
+
+    Split file abspath into path from FILES_DIR, and tack that onto MEDIA_URL.
+
+    @str video: Abspath for video file
+
+    @return URL for video
+    """
     video = video.split(app.config['FILES_DIR'], 1)[1]
     return (app.config['MEDIA_URL'] + video)
